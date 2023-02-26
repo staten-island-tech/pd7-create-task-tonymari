3262d65b7ca5c7a7a3af4b0f89aebc55096c3b4e
tonymari <113132219+tonymari@users.noreply.github.com>
2023-02-26 06:55:51 -0500

    jnkjdcjk

--- create-task/java/data.js
+++ create-task/java/data.js
@@ -1,6 +1,17 @@
 var questions = $(".question").length;
 var total = 0;
 var average = 0;
+var myQuestions = $("section.q-n-a");
+
+shuffle(myQuestions);
+console.log(myQuestions);
+
+function shuffle(array) {
+  for (let i = array.lenght - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], (array[j] = array[j]), array[i]];
+  }
+}
 
 $(".answer").on("click", function () {
   if ($(this).parent().find(".selected").length > 0) {
@@ -16,13 +27,19 @@ $(".answer").on("click", function () {
 $(".finish").on("click", function () {
   avg = total / questions;
   var message = "";
-  if (avg < 1.5) {
-    message = "you are thomas";
-  } else if (avg < 2.5) {
-    ("You are percy");
-  } else if (avg < 3.5) {
-    ("You are james");
+  if ($("selected").length === questions) {
+    if (avg < 1.5) {
+      message = "you are thomas";
+    } else if (avg < 2.5) {
+      ("You are percy");
+    } else if (avg < 3.5) {
+      ("You are james");
+    } else {
+      ("You are toby");
+    }
   } else {
-    ("You are toby");
+    message = "oh no go back you missed a question";
   }
+
+  alert(message);
 });
