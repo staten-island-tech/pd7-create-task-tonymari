9b52f89064246d40528958d510112757ca387846
tonymari <113132219+tonymari@users.noreply.github.com>
2023-02-26 12:24:20 -0500

    frj

---
+++ create-task/js/array.js
@@ -0,0 +1,25 @@
+const questions = [
+  {
+    id: 1,
+    question:
+      "Your train is tired from a long day of work, they choose to relax in",
+    answers: [
+      {
+        text: "Tidmouth Sheds",
+        cat: "Thomas","Percy","James","Gordon":
+      },
+      {
+        text: "Think it's more important to give them some of your time; work can wait",
+        cat: "extrovert",
+      },
+      {
+        text: "Listen, but with only with half an ear",
+        cat: "introvert","extrovert"
+        :"".dfjb
+      },
+      {
+        text: "Interrupt and explain that you are really busy at the moment",
+        cat: "extrovert",
+      },
+    ],
+  },
--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,8 +1,6 @@
-import "../styles/style.css";
-import { DOMSelectors } from "./dom";
-
 var questions = $(".question").length;
 var total = 0;
+var average = 0;
 var avg = 0;
 var myQuestions = $("section.q-n-a");
 
@@ -27,28 +25,3 @@ $(".answer").on("click", function () {
     total -= $(this).parent().find(".selected").data("value");
     $(this).parent().find(".selected").removeClass("selected");
   }
-  $(this).addClass("selected");
-  total += $(this).data("value");
-
-  console.log(total);
-});
-
-$(".finish").on("click", function () {
-  avg = total / questions;
-  var message = "";
-  if ($("selected").length === questions) {
-    if (avg < 1.5) {
-      message = "you are thomas";
-    } else if (avg < 2.5) {
-      ("You are percy");
-    } else if (avg < 3.5) {
-      ("You are james");
-    } else {
-      ("You are toby");
-    }
-  } else {
-    message = "oh no go back you missed a question";
-  }
-
-  alert(message);
-});
