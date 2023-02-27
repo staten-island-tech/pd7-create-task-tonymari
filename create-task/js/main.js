bcefb40d8cc5b1dc00c12aaa684a08c4a6bca51b
tonymari <113132219+tonymari@users.noreply.github.com>
2023-02-27 06:39:49 -0500

    ajbflej

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,4 +1,4 @@
-(".question").length;
+var questions = $(".question").length;
 var total = 0;
 var avg = 0;
 var myQuestions = $("section.q-n-a");
@@ -23,4 +23,29 @@ $(".answer").on("click", function () {
   if ($(this).parent().find(".selected").length > 0) {
     total -= $(this).parent().find(".selected").data("value");
     $(this).parent().find(".selected").removeClass("selected");
-  }}
+  }
+  $(this).addClass("selected");
+  total += $(this).data("value");
+
+  console.log(total);
+});
+
+$(".finish").on("click", function () {
+  avg = total / questions;
+  var message = "";
+  if ($("selected").length === questions) {
+    if (avg < 1.5) {
+      message = "you are thomas";
+    } else if (avg < 2.5) {
+      ("You are percy");
+    } else if (avg < 3.5) {
+      ("You are james");
+    } else {
+      ("You are toby");
+    }
+  } else {
+    message = "oh no go back you missed a question";
+  }
+
+  alert(message);
+});
